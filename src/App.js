9186ffeb18305d5bf47479ee0406eb8925b15baa
ChrisKoetzee <chriskoetzee81@mail.com>
2024-06-20 16:02:49 +0200

    setting up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import { Button } from 'react-bootstrap';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h3>Hi, my name is </h3>
+//         <h1><strong>Christopher Koetzee</strong></h1>
+//         <h1>Full stack Developer !!!</h1>
+//         <h2>Welcome to my Portfolio Page</h2>
+//         <Button variant="primary">Primary Button</Button>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from 'react';
+import { MDBBtn, MDBContainer, MDBAlert } from 'mdb-react-ui-kit';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MDBContainer className="mt-5">
+      <h1>Welcome to MDB React</h1>
+      <MDBBtn color="primary">Primary Button</MDBBtn>
+      <MDBAlert color="success" className="mt-3">
+        This is a success alert!
+      </MDBAlert>
+    </MDBContainer>
   );
 }
 
